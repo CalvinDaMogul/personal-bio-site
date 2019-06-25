5a2ced67e8be5329c8c011fea2d9aa6fbc306de3
Calvin J Foster <cal.j.foster@gmail.com>
2019-06-25 17:55:21 -0500

    projects print

---
+++ src/javascripts/helpers/Data/mainData.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import apiKeys from './apiKeys.json';
+
+const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getAssignmentsByUid = () => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/assignments.json`)
+    .then((results) => {
+      const assignmentResults = results.data;
+      const assignments = [];
+      Object.keys(assignmentResults).forEach((assignmentsId) => {
+        assignmentResults[assignmentsId].id = assignmentsId;
+        assignments.push(assignmentResults[assignmentsId]);
+      });
+      resolve(assignments);
+      console.error(assignments);
+    })
+    .catch(err => reject(err));
+});
+
+export default { getAssignmentsByUid };
